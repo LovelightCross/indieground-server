135dc34769dae25ba8175fb407e1b6d8f6ac21c8
BRPickens <brpickens21@gmail.com>
2017-12-05 15:16:27 -0600

    lets go

--- src/controllers/venues.ctrl.js
+++ src/controllers/venues.ctrl.js
@@ -1,14 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const users_proc_1 = require("../procedures/users.proc");
+const venues_proc_1 = require("../procedures/venues.proc");
 exports.all = (req, res, next) => {
-    users_proc_1.default.all()
+    venues_proc_1.default.all()
         .then((sets) => {
         res.json(sets);
     });
 };
 exports.create = (req, res, next) => {
-    users_proc_1.default.create(req.body.handle, req.body.username, req.body.email, req.body.password)
+    venues_proc_1.default.create(req.body.venueId, req.body.name, req.body.address1, req.body.address2, req.body.city, req.body.state, req.body.zip)
         .then((sets) => {
         res.json(sets);
     });
